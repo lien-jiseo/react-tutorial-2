4852769527260eb80b6f543ba9f135f44e217de5
lien-jiseo <jiin@lien.kr>
2022-06-20 16:48:28 +0900

    Game handleClick 수정
    -     const history = this.state.history.slice(0, this.state.stepNumber + 1); 으로 변경
    - stepNumber: history.length, 추가

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -60,7 +60,7 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -69,6 +69,7 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       history: history.concat([{ squares: squares }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
