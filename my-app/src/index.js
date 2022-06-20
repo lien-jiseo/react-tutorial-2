145af038887988c7f57c7f7cacaebda64970f9af
lien-jiseo <jiin@lien.kr>
2022-06-20 12:12:41 +0900

    Board constructor 삭제, renderSquare 변경
    - renderSquare에 value={this.props.squares[i]} 으로 변경
    - onClick={() => this.props.onClick(i)} 으로 변경

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -13,14 +13,6 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
   handleClick(i) {
     const squares = this.state.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -33,8 +25,8 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
